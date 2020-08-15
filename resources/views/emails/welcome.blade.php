543c01caa2623e982f211a838f57078db774884e
Gerasimov1991 <svt.triya1991@gmail.com>
2020-08-15 23:33:23 +1000

    update

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -7,7 +7,7 @@
                     <!-- section text ======-->
 
                     <p style="font-size:17px; color:#fff; margin:10px; 0">
-                    Hello {{ $username }}
+                    Hello 
                     </p>
                     
                     <p style="font-size:17px; color:#fff; margin:10px; 0">
